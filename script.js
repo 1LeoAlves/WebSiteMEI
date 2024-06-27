d948ce52677436f5f634304a6a75949f804dd058
anaperes94 <162990868+anaperes94@users.noreply.github.com>
2024-06-27 10:13:56 -0300

    Site final

--- script.js
+++ script.js
@@ -40,3 +40,54 @@ if(window.location.pathname == "/cart.html"){
         e.returnValue = '';
     });
 }
+function configurarMostrarConteudoInicial() {
+    const contents = document.querySelectorAll('.nav-content');
+
+    // Mostra o primeiro conteúdo inicialmente
+    if (contents.length > 0) {
+        contents[0].classList.add('active');
+    }
+}
+function configurarMostrarConteudoInicial() {
+    const contents = document.querySelectorAll('.nav-content');
+
+    // Mostra o primeiro conteúdo inicialmente
+    if (contents.length > 0) {
+        contents[0].classList.add('active');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navItems = document.querySelectorAll('.nav-item');
+
+    // Adiciona evento de clique a cada item de navegação
+    navItems.forEach(function(item) {
+        const link = item.querySelector('.nav-button');
+        const underline = item.querySelector('.underline');
+
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Previne o comportamento padrão do link
+            const currentActive = document.querySelector('.nav-item.active');
+
+            // Remove a classe 'active' do item atualmente ativo
+            if (currentActive) {
+                currentActive.classList.remove('active');
+                currentActive.querySelector('.underline').style.width = '0'; // Esconde a linha do item não selecionado
+            }
+
+            // Adiciona a classe 'active' ao item clicado
+            item.classList.add('active');
+
+            // Ajusta a posição da linha indicadora
+            const linkRect = link.getBoundingClientRect();
+            const navRect = document.getElementById('nav-list').getBoundingClientRect();
+            underline.style.width = `${link.offsetWidth}px`;
+        });
+    });
+});
+
+function CheckPage(pathname){
+    if(pathname != 'http://127.0.0.1:5500/WebSiteMEI-main/index.html'){
+        window.location.href = '//127.0.0.1:5500/WebSiteMEI-main/index.html';
+    }
+}
